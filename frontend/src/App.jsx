5f7020b554515279d732efb91669ef1274ed0acb
Gregory <Gregory.cormier.ets@gmail.com>
2022-10-26 14:14:03 +0200

    Création de la page résultat par Grégory et Malo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 import Quiz from './pages/Quiz'
+import Result from './pages/Result'
 
 function App () {
   return (
@@ -9,6 +10,7 @@ function App () {
       <Routes>
         <Route path='/' index element={<Home />} />
         <Route path='/quiz' index element={<Quiz />} />
+        <Route path='/Result' index element={<Result />} />
       </Routes>
     </div>
   )
