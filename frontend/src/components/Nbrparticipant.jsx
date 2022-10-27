9be3a54fb1135adc777cd4c453fbbfd7f077e012
Victor grlr <victor.grollier@gmail.com>
2022-10-27 13:28:59 +0200

    Victor Count Player

--- frontend/src/components/Nbrparticipant.jsx
+++ frontend/src/components/Nbrparticipant.jsx
@@ -3,8 +3,8 @@ import '../styles/NbrparticipantStyle.css'
 function Nbrparticipant (props) {
   return (
     <div className='joueurconnecte'>
-       <p><strong>Connectez-vous,</strong></p>
-       <p className='textedif'><strong>X</strong> joueurs déjà en jeu !</p>
+      <p><strong>Connectez-vous,</strong></p>
+      <p className='textedif'><strong>{props.players}</strong> joueurs déjà en jeu !</p>
     </div>
   )
 }
