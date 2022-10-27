a57990aa388cfe7ffaf51e63f365d845bdee8ee9
Antonin <valableantonin@gmail.com>
2022-10-27 13:41:07 +0200

    Question sur Projection

--- frontend/src/pages/Projection.jsx
+++ frontend/src/pages/Projection.jsx
@@ -6,29 +6,63 @@ import NumberQuestion from '../components/NumberQuestion'
 import TextQuestion from '../components/TextQuestion'
 import Podium from '../components/Podium'
 import Header from '../components/Header'
+import Button from '../components/Button'
+import ProgressBar from '../components/ProgressBar'
+import { useEffect, useState } from 'react'
+import { getQuestions } from '../services/Api'
 
 function Projection () {
+  const [questions, setQuestions] = useState([])
+  const [index, setIndex] = useState(0)
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await getQuestions()
+      if (data && data.data) {
+        setQuestions(data.data)
+      }
+    }
+    getData()
+  }, [])
+
+  const handleNext = () => {
+    if (index < questions.length - 1) {
+      setIndex(index + 1)
+    }
+  }
+
+  const handlePrevious = () => {
+    if (index > 0) {
+      setIndex(index - 1)
+    }
+  }
+
   return (
     <div className='container'>
       <Header />
       <div className='row'>
 
         <div className='col leftside'>
-          <Leftside />
-          <Lienconnexion />
-          <Nbrparticipant />
+          <div className='question-project'>
+            <ProgressBar length={questions.length} progress={index + 1} />
+
+            <NumberQuestion number={index + 1} />
+            <TextQuestion question={questions[index].attributes.content} />
+
+            <Button onClick={handlePrevious} text='<' />
+            <Button onClick={handleNext} text='>' />
+          </div>
+          <h4 className='classement'>Classement</h4>
+          <div className='border'> </div>
+          <Podium />
         </div>
 
         <div className='col centerside'> </div>
 
         <div className='col rightside'>
-          <div className='question-project'>
-            <NumberQuestion number='Question 1' />
-            <TextQuestion question='Dans quelle ville l’ESP et l’ESD ne sont pas présente ? ' />
-          </div>
-          <h4 className='classement'>Classement</h4>
-          <div className='border'> </div>
-          <Podium />
+          <Leftside />
+          <Lienconnexion />
+          <Nbrparticipant />
         </div>
 
       </div>
